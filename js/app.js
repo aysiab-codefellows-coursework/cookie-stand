ab34d764c23b0c135afb6936d5444ed0b86d706a
Aysia Brown <aysiaebrown@gmail.com>
2020-09-07 13:07:50 -0700

    created object literals for each location in js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,36 @@
+// object literals for each location 
 
+var storeSeattle = {
+    location: "Seattle",
+    minCust: 23,
+    maxCust: 65, 
+    avgSale: 6.3,
+}
+
+var storeTokyo = {
+    location: "Tokyo",
+    minCust: 3,
+    maxCust: 24,
+    avgSale: 1.2,
+}
+
+var storeDubai = {
+    location: "Dubai",
+    minCust: 11,
+    maxCust: 38,
+    avgSale: 3.7,
+}
+
+var storeParis = {
+    location: "Paris",
+    minCust: 20,
+    maxCust: 38,
+    avgSale: 2.3,
+}
+
+var storeLima = {
+    location: "Lima",
+    minCust: 2,
+    maxCust: 16,
+    avgSale: 4.6,
+}
